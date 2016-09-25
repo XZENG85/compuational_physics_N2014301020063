6ba5869457b2d05108ff5613f2aeb4d2995fde6f
allenoel <505654339@qq.com>
2016-09-26 00:07:51 +0800

    Create Exercise_03_2.py

---
+++ Codes/Exercise_03_2.py
@@ -0,0 +1,58 @@
+import time 
+import os
+os.system('cls')
+print("This is a spider")
+time.sleep(1)
+os.system('cls')
+a01=" #                               # "
+a02="    #            #            #    "
+a03="　     #         #         #       "
+a04="　　　　  #　     #      #          "
+a05="           　# # # # #             "
+a06="          #         　　#          "
+a07="# # # # # #         　　# # # # # #"
+a08="          #         　　#          "
+a09="           　# # # # #             "
+a10="          #             #          "
+a11="       #                   #       "
+a12="    #                         #    "
+a13=" #                               # "
+a=[a01,a02,a03,a04,a05,a06,a07,a08,a09,a10,a11,a12,a13]
+b01="          #           "
+b02="#         #         # "
+b03="  #       #       #   "
+b04="    #     #     #     "
+b05="      #   #   #       "
+b06="        # # #         "
+b07="      #   　  #       "
+b08="      #   　  #       "
+b09="      #       # # # # "
+b10="      #       #       "
+b11="      #       #       "
+b12="        # # #         "
+b13="      #　 #   #       " 
+b14="    #     #     #     "
+b15="  #       #       #   "
+b16="#         #         # "
+b17="          #           "
+b=[b01,b02,b03,b04,b05,b06,b07,b08,b09,b10,b11,b12,b13,b14,b15,b16,b17]
+for i in a:
+    print(i)
+time.sleep(0.5)   
+i=os.system('cls')
+for i in b:
+    print(i)
+time.sleep(0.5)   
+i=os.system('cls')
+a.reverse()
+for i in a:
+    print(i)
+time.sleep(0.5)   
+i=os.system('cls')
+b.reverse()
+for i in b:
+    print(i)
+time.sleep(0.5)   
+i=os.system('cls')
+for i in a:
+    print(i)
