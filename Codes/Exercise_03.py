a03a96c94c5fe4b3701c62bf99c493ac0ec83c9c
allenoel <505654339@qq.com>
2016-09-25 16:53:48 +0800

    Create Exercise_03.py

---
+++ Codes/Exercise_03.py
@@ -0,0 +1,14 @@
+import time 
+import os
+os.system('cls')
+print("My English name is 'Noel'")
+time.sleep(1)
+os.system('cls')
+for i in range(0,50):
+    print(i*" "+"#    #     ####     #####    #     ")
+    print(i*" "+"##   #    #    #    #        #     ")
+    print(i*" "+"# #  #    #    #    #####    #     ")
+    print(i*" "+"#  # #    #    #    #        #     ")
+    print(i*" "+"#    #     ####     #####    ##### ") 
+    time.sleep(0.15)
+    os.system('cls')
