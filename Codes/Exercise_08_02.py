d591876cd5e306b99e9ca17add6f4489f60bbf2d
allenoel <505654339@qq.com>
2016-11-14 00:23:26 +0800

    Update Exercise_08_02.py

--- Codes/Exercise_08_02.py
+++ Codes/Exercise_08_02.py
@@ -1,47 +1,50 @@
+from __future__ import division
 import math
 import pylab as pl
-g=9.8
-l=9.8
-q=0.5
-ou_D=2/3
-class chaos:
-    def __init__(self,F_D,sita):
-        self.sita=[sita]
-        self.omiga=[0]
-        self.t=[0]
-        self.dt=(3*(math.pi))/1000
-        self.F_D=F_D
-        self.omiga2=[]
-        self.sita2=[]
-    
-    def calculate(self):
-        while self.t[-1]<6000:
-            omiga_new=self.omiga[-1]-((g/l)*(math.sin(self.sita[-1]))+q*self.omiga[-1]-self.F_D*math.sin(ou_D*self.t[-1]))*self.dt
-            self.omiga.append(omiga_new)
-            sita_new=self.sita[-1]+self.omiga[-1]*self.dt
-            t_new=self.t[-1]+self.dt
-            
-            while sita_new > math.pi:
-                sita_new -=2*(math.pi)
-            while sita_new < -math.pi:
-                sita_new +=2*(math.pi)       
-            self.sita.append(sita_new)
-            self.t.append(t_new)
-            
-F_D2=[1.35]*128
-sita3=[]           
-while F_D2[-1]<1.5:
-    A=chaos(F_D2[-1],0.2)
-    A.calculate()
-    sita2=[]
-    for i in range(301,429):
-        A.sita2.append(A.sita[1000*i])
-    sita3=sita3+A.sita2
-    F_Dnew=[F_D2[-1]+0.0001]*128
-    F_D2=F_D2+F_Dnew
-    print('q')        
-pl.plot(F_D2[0:-128],sita3,'r.')
-pl.title(r'$\omega$ versus $\theta$')
-pl.xlabel(r'$\theta$(rad)')
-pl.ylabel(r'$\omega$(rad/s)')
-pl.show() 
+import numpy
+# define a function that given amplitude of force, gives angle,avelo and t
+def change_amp(F):   
+    # define some constants
+    q = 0.5
+    l = 9.8
+    g = 9.8
+    f = 2/3
+    dt = math.pi/100
+    theta0 = 0.2
+    omega0 = 0
+    angle = [theta0]
+    avelo = [omega0]
+    t = [0]
+    an=[]
+    # move the pendulum
+    while t[-1] <= 1200*math.pi:
+        avelo_new = avelo[-1] - ((g/l)*math.sin(angle[-1]) + q*avelo[-1] - F*math.sin(f*t[-1]))*dt
+        avelo.append(avelo_new)
+        angle_new = angle[-1] + avelo[-1]*dt
+        while angle_new > math.pi:
+            angle_new -= 2*math.pi
+        while angle_new < -math.pi:
+            angle_new += 2*math.pi
+        angle.append(angle_new)
+        if t[-1]>=900*math.pi:
+            if t[-1]%(3*math.pi)<=dt:
+                an.append(angle_new)
+        t_new = t[-1] + dt
+        t.append(t_new)
+    return an
+fd1=list(numpy.linspace(1.35,1.5,100))
+fd=[]
+th=[]
+for i in fd1:
+    points=change_amp(i)
+    for j in points:
+        fd.append(i)
+        th.append(j)
+pl.scatter(fd,th,s=10)
+pl.grid(True)
+pl.xlim(1.35,1.48)
+pl.ylim(0,3)
+pl.title('Bifurcation diagram')
+pl.xlabel('FD')
+pl.ylabel('theta(radians)')
+pl.show()
